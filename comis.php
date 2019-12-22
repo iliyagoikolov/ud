58f2c30a4b0c404bd46e07ec3434a6a0f54f9175
Ilya <iliyagoikolov@gmail.com>
2019-12-22 23:03:00 +0300

    modified comis.php

--- comis.php
+++ comis.php
@@ -40,6 +40,12 @@
                     echo "<option value = '$row->faculty'>$row->faculty</option>";
                 }
 
+                ?>
+    </select>
+    <?php
+    echo  "<button \"type=\"submit\" name=\"name\" class='edit btn btn-warning mb-2 mt-2' value = \"\">
+     Выдать отчет</button>";
+        if ($faculty !='') {
             $sql ="SELECT COUNT(*) as count,`departments`.`department` FROM `entrants` JOIN
                 `departments` ON `entrants`.`department`=`departments`.`department`
                 WHERE `departments`.`faculty` = :faculty
@@ -50,81 +56,77 @@
                      $departments_count[$row->department] = $row->count;
                  }
 
-                ?>
-    </select>
-    <?php
-    echo  "<button \"type=\"submit\" name=\"name\" class='edit btn btn-warning mb-2 mt-2' value = \"\">
-     Выдать отчет</button>";
-     echo "<font size=\"5\">";
-         echo "<p> Количество посупающих на факультет: <b>$count_faculty</b>чел. </p>";
-          echo "<div class='bg-primary rounded p-1'><p> Количество посупающих на кафедры:</p>";
-          foreach ($departments_count as $dep => $c) {
-              echo "<p> $dep: <b>$c</b>чел. </p>";
-          }
-          echo "</div>";
-          echo "<div class='bg-warning rounded p-1 my-2'>";
-          foreach ($departments_count as $dep => $c) {
-              $sql ="SELECT COUNT(*) as count, `entrants`.`group_number` FROM `entrants`
-               JOIN `group_department` ON `entrants`.`group_number`=`group_department`.`group_number`
-                WHERE `entrants`.`department`=:dep group by `entrants`.`group_number`";
-                $query = $pdo->prepare($sql);
-                $query->execute(['dep'=>$dep]);
 
-                echo "<p><b>Для  $dep:</b></p>";
-                while ($row = $query->fetch(PDO::FETCH_OBJ)){
-                    echo "<p>Группа №$row->group_number - $row->count чел.</p>";
-                }
-          }
-          echo "</div>";
-          echo "<div class='bg-success text-white rounded p-1 my-2'>";
-          echo "<p align = \"center\">Экзамены:</p>";
-          foreach ($departments_count as $dep => $c) {
-              $sql = "SELECT * FROM `department_exam` JOIN `exams` ON
-              `department_exam`.`exam_id`=`exams`.`exam_id` WHERE
-              `department_exam`.`department` =:dep";
-              $query = $pdo->prepare($sql);
-              $query->execute(['dep'=>$dep]);
-              echo "<ol><p><b>Список экзаменов для $dep:</b></p>";
-              while ($row = $query->fetch(PDO::FETCH_OBJ)){
-                  echo "<p><li>$row->exam => дата: $row->date => аудитория: $row->audience</li></p>";
-              }
-              echo "</ol><br>";
-          }
-          echo "</div>";
-          echo "<br><p align = \"center\">Сколько студентов сдало на оценки по предметам:</p>";
-          $sql = "SELECT `exams`.`exam`, COUNT(*) as count FROM `sheet` JOIN `exams` ON
-          `sheet`.`exam_id` = `exams`.`exam_id` JOIN `departments` ON
-          `sheet`.`department` = `departments`.`department` WHERE
-           `departments`.`faculty` = :faculty &&
-           `sheet`.`mark` = :mark GROUP BY `exams`.`exam`";
-           echo "<table class=\"table table-danger table-bordered\">
-               <thead class=\"thead thead-dark align-content-center\">
-                   <tr>
-                       <th>Экзамен</th>
-                       <th>Оценка</th>
-                       <th>Количество оценок</th>
-                   </tr>
-               </thead>
-               <tbody>";
+             echo "<font size=\"5\">";
+                 echo "<p> Количество посупающих на факультет: <b>$count_faculty</b>чел. </p>";
+                  echo "<div class='bg-primary rounded p-1'><p> Количество посупающих на кафедры:</p>";
+                  foreach ($departments_count as $dep => $c) {
+                      echo "<p> $dep: <b>$c</b>чел. </p>";
+                  }
+                  echo "</div>";
+                  echo "<div class='bg-warning rounded p-1 my-2'>";
+                  foreach ($departments_count as $dep => $c) {
+                      $sql ="SELECT COUNT(*) as count, `entrants`.`group_number` FROM `entrants`
+                       JOIN `group_department` ON `entrants`.`group_number`=`group_department`.`group_number`
+                        WHERE `entrants`.`department`=:dep group by `entrants`.`group_number`";
+                        $query = $pdo->prepare($sql);
+                        $query->execute(['dep'=>$dep]);
 
-          for ($i=2; $i <= 5 ; $i++) {
-              $query = $pdo->prepare($sql);
-              $query->execute(['faculty'=>$faculty,'mark'=>$i]);
+                        echo "<p><b>Для  $dep:</b></p>";
+                        while ($row = $query->fetch(PDO::FETCH_OBJ)){
+                            echo "<p>Группа №$row->group_number - $row->count чел.</p>";
+                        }
+                  }
+                  echo "</div>";
+                  echo "<div class='bg-success text-white rounded p-1 my-2'>";
+                  echo "<p align = \"center\">Экзамены:</p>";
+                  foreach ($departments_count as $dep => $c) {
+                      $sql = "SELECT * FROM `department_exam` JOIN `exams` ON
+                      `department_exam`.`exam_id`=`exams`.`exam_id` WHERE
+                      `department_exam`.`department` =:dep";
+                      $query = $pdo->prepare($sql);
+                      $query->execute(['dep'=>$dep]);
+                      echo "<ol><p><b>Список экзаменов для $dep:</b></p>";
+                      while ($row = $query->fetch(PDO::FETCH_OBJ)){
+                          echo "<p><li>$row->exam => дата: $row->date => аудитория: $row->audience</li></p>";
+                      }
+                      echo "</ol><br>";
+                  }
+                  echo "</div>";
+                  echo "<br><p align = \"center\">Сколько студентов сдало на оценки по предметам:</p>";
+                  $sql = "SELECT `exams`.`exam`, COUNT(*) as count FROM `sheet` JOIN `exams` ON
+                  `sheet`.`exam_id` = `exams`.`exam_id` JOIN `departments` ON
+                  `sheet`.`department` = `departments`.`department` WHERE
+                   `departments`.`faculty` = :faculty &&
+                   `sheet`.`mark` = :mark GROUP BY `exams`.`exam`";
+                   echo "<table class=\"table table-danger table-bordered\">
+                       <thead class=\"thead thead-dark align-content-center\">
+                           <tr>
+                               <th>Экзамен</th>
+                               <th>Оценка</th>
+                               <th>Количество оценок</th>
+                           </tr>
+                       </thead>
+                       <tbody>";
 
-              while ($row = $query->fetch(PDO::FETCH_OBJ)){
-                   echo "<tr>";
-                 echo "<td><p>$row->exam</p></td>
-                  <td><p>$i</p></td>
-                  <td><p>$row->count</p></td>";
-                   echo "</tr>";
-              }
+                  for ($i=2; $i <= 5 ; $i++) {
+                      $query = $pdo->prepare($sql);
+                      $query->execute(['faculty'=>$faculty,'mark'=>$i]);
 
-          }
+                      while ($row = $query->fetch(PDO::FETCH_OBJ)){
+                           echo "<tr>";
+                         echo "<td><p>$row->exam</p></td>
+                          <td><p>$i</p></td>
+                          <td><p>$row->count</p></td>";
+                           echo "</tr>";
+                      }
 
-          echo "</tbody>
-          </table>";
-    echo "</font>";
+                  }
 
+                  echo "</tbody>
+                  </table>";
+            echo "</font>";
+        }
       ?>
         </form>
 
